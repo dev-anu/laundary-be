6bed110605c53ec6b9c3e2bc03a6d07ff5811214
dev-anu <anu.infy850@gmail.com>
2024-11-21 23:27:14 +0530

    adding default route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express ,{Application} from "express";
 import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
+
 
 const app:Application=express();
 
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//Routes
+
+
+//swagger setup
 const swaggerOptions={
     swaggerDefinition:{
         openapi:'3.0.0',
@@ -17,6 +27,13 @@ const swaggerOptions={
 }
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+//swaggers-route
 app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+//Default Route
+app.get('/',(req,res)=>{
+    res.send('Welcome to the On-Demand Laundary App Api!')
+})
 
 export default app;
