992dbc75ec5ee1450c2c73494098b523805ae763
dev-anu <anu.infy850@gmail.com>
2024-11-21 23:19:50 +0530

    setup mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
+import mongoose from "mongoose";
 import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+//database setup
+const connectDB = async()=>{
+    try{
+        const mongoURI= process.env.MONGO_URI || 'mongodb://localhost:27017/laundary-app';
+        await mongoose.connect(mongoURI);
+    }catch(err){
+        console.error('Error connection to MongoDB:',err);
+        process.exit(1);
+    }
+}
+
 
 const PORT = process.env.PORT || 5000;
-
+connectDB().then(()=>{
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
+})
 })
